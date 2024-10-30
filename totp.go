5fec914eb7f4462053bfbc7fa170a895234be640
binh.hv <binh.hv@hsc.com.vn>
2024-10-30 08:20:05 +0700

    test

--- totp.go
+++ totp.go
@@ -2,14 +2,9 @@ package totp
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/pquerna/otp/totp"
 	"go.k6.io/k6/js/modules"
 )
-
-// TOTP structure
-type TOTP struct{}
 
 // init function to register the TOTP module
 func init() {
@@ -17,16 +12,9 @@ func init() {
 	modules.Register("k6/x/totp", new(TOTP))
 }
 
+// TOTP structure
+type TOTP struct{}
+
 func (*TOTP) Hello(name string) string {
 	return "Hello " + name
-}
-
-// Generate function to create a TOTP token based on a secret
-func (t *TOTP) Generate(secret string) (string, error) {
-	fmt.Println("Generate method called") // Debugging line
-	token, err := totp.GenerateCode(secret, time.Now())
-	if err != nil {
-		return "", err
-	}
-	return token, nil
 }
