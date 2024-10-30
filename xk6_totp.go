0561a2cdb58b10c2c1ed6a25338bafc216462c1c
binh.hv <binh.hv@hsc.com.vn>
2024-10-30 08:30:08 +0700

    test

---
+++ xk6_totp.go
@@ -0,0 +1,22 @@
+package xk6_totp
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pquerna/otp/totp"
+	"go.k6.io/k6/js/modules"
+)
+
+// init function to register the TOTP module
+func init() {
+	fmt.Println("Initializing x TOTP module")
+	modules.Register("k6/x/xk6_totp", new(TOTP))
+}
+
+// TOTP structure
+type TOTP struct{}
+
+func (*TOTP) Hello(name string) string {
+	return "Hello " + name
+}
